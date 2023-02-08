5be51f072580f53fddc86e6e52e2f86525f0522b
Olivier Lamarre <7255017+olamarre@users.noreply.github.com>
2023-02-07 20:10:13 -0500

    Custom package modules

--- hello_python/calculator.py
+++ hello_python/calculator.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
 
-def add(i, j):
+""" 
+    A set of calculator functions
+
+    Author: <your name, <email>>
+    Affl.: <your affiliation>
+"""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def add(i,j):
+    """Add two numbers"""
+    log.info(f"Adding {i} and {j}")
     return i+j
+
+def subtract(i,j):
+    """Subtract the second number from the first"""
+    log.info(f"Subtracting {j} from {i}")
+    return i-j
+
+def multiply(i,j):
+    """Multiply two numbers"""
+    log.info(f"Multiplying {i} and {j}")
+    return i*j
+
+def divide(i,j):
+    """Divide the first number by the second, which must be non-zero"""
+    log.info(f"Diving {i} by {j}")
+    return i/j
---
+++ hello_python/foo_subpackage/bar.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+""" 
+    <Short description of the bar module>
+
+    Author: <yourself, <email>>
+    Affl.: <your affiliation>
+"""
