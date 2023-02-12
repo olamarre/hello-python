0aaf3731b42b8e4a0658002dd8e98db680635de1
Olivier Lamarre <7255017+olamarre@users.noreply.github.com>
2023-02-11 20:19:19 -0500

    Created config dir

--- hello/dev.py
+++ hello/dev.py
@@ -16,9 +16,11 @@ REPO_PATH = Path(__file__).resolve().parents[1]
 class _Config:
     root_dir = REPO_PATH
     data_dir = Path(REPO_PATH, 'data')
+    config_dir = Path(REPO_PATH, 'config')
 
 print(f"Repository root directory: {_Config.root_dir}")
 print(f"Repository data directory: {_Config.data_dir}")
+print(f"Repository config directory: {_Config.config_dir}")
 
 REPO_CFG = _Config()
 
